4b3cea8b5b77fee0b2ca4c3c48d4093e013bf86e
Muhamadirfanm134 <35161809+Muhamadirfanm134@users.noreply.github.com>
2022-10-10 16:42:31 +0700

    style(footer): Add Card Background Image
    style(footer): Add Card Background Image
    
    Add background image for header card.
    
    Closes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import searchIcon from "./icons/searchIcon.svg";
 import notFound from "./icons/notFound.svg";
+import background from "./images/background.png";
 
-export { notFound, searchIcon };
+export { notFound, searchIcon, background };
